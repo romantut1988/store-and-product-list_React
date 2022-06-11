a00ddb5f7be25c9e1b52ae9809210d11c211f4d8
romantut1988 <romantut1988@mail.ru>
2022-06-12 01:05:26 +0300

    header properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 function App() {
   return (
-    <div className="App">
-     Hello !!!
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" />
+          <svg />
+          <div className="headerInfo">
+            <h3>React sneakers</h3>
+            <p>Магазин лучших крассовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <svg />
+            <span>1205 руб.</span></li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+          <h1>Все красовки</h1>
+          ..........
+      </div>
     </div>
   );
 }
