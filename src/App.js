70eca2fd6fe3283b7589fe5b7a49d4869b3251bc
romantut1988 <romantut1988@mail.ru>
2022-06-15 00:48:07 +0300

    breaking down tags into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 function App() {
   return (
     <div className="wrapper">
+      <div style={{display: 'none'}} className="overlay">
+        <div className="drawer">
+          <h2>Корзина <img className="removeBtn" src="/img/btn-remove.svg" alt="Remover" /></h2>
+          <div className="items">
+
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+              <div className="cartPrice">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remover" />
+            </div>
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+              <div className="cartPrice">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remover" />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">оформить заказ <img src="/img/arrow.svg" alt="Arrow"/></button>
+          </div>
+        </div>
+        </div>
+
+
       <header>
         <div className="headerLeft">
           <img width={40} height={40} src="/img/logo.png" />
@@ -30,7 +71,7 @@ function App() {
         <div className="sneakers">
           <div className="card">
             <div className="favorite">
-            <img src="/img/hearn-unlicked.svg" alt="Unliked" />
+              <img src="/img/hearn-unlicked.svg" alt="Unliked" />
             </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -70,6 +111,7 @@ function App() {
               </button>
             </div>
           </div>
+           
           <div className="card">
             <img width={133} height={112} src="/img/sneakers/4.jpg" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
