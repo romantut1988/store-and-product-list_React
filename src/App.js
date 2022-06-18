50d476f37eb163c91d258ddb39ded207ede8788a
romantut1988 <romantut1988@mail.ru>
2022-06-18 17:55:50 +0300

    product search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Drawer } from './components/Drawer';
 function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const [cartOpened, setCartOpened] = useState(false);
 
   useEffect(() => {
@@ -22,21 +23,30 @@ function App() {
     setCartItems(prev => [...prev, obj]);
   }
 
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="wrapper">
       {cartOpened && <Drawer items={cartItems} onClose={() => { setCartOpened(false) }} />}
       <Header onClickCatr={() => setCartOpened(true)} />
       <div className="content">
         <div className="contentInput">
-          <h1>Все крассовки</h1>
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
           <div className="search-block">
             <img src="/img/search.svg" alt="Search" />
-            <input className="inputSearch" placeholder="Поиск..." />
+            {searchValue && <img onClick={ () => setSearchValue('')} className="removeBtn" src="/img/btn-remove.svg" alt="Clear" />}
+            <input onChange={onChangeSearchInput} value={searchValue} className="inputSearch" placeholder="Поиск..." />
           </div>
         </div>
         <div className="sneakers">
-          {items.map((item) => (
-            <Card title={item.title}
+          {items
+          .filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((item, index) => (
+            <Card
+              key={index}
+              title={item.title}
               price={item.price}
               imageUrl={item.imageUrl}
               onFavotite={() => console.log('Добавили в закладки')}
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export function Header(props) {
     return (
         <header>
             <div className="headerLeft">
-                <img width={40} height={40} src="/img/logo.png" />
+                <img width={40} height={40} src="/img/logo.png" alt="Logotype"/>
                 <div className="headerInfo">
                     <h3>React sneakers</h3>
                     <p>Магазин лучших крассовок</p>
@@ -10,10 +10,10 @@ export function Header(props) {
             </div>
             <ul className="headerRight">
                 <li onClick={props.onClickCatr}>
-                    <img width={18} height={18} src="/img/cart.svg" />
+                    <img width={18} height={18} src="/img/cart.svg" alt="Cart"/>
                     <span>1205 руб.</span></li>
                 <li>
-                    <img width={18} height={18} src="/img/user.svg" />
+                    <img width={18} height={18} src="/img/user.svg" alt="User"/>
                 </li>
             </ul>
         </header>
